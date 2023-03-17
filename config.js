388548b535b85f2ee27957ff844e1680088c3346
Ȥᴇᴇsʜᴀɴ ƛɴsᴀʀI <107912387+Z335H4N8@users.noreply.github.com>
2023-03-17 11:27:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*𝙼𝙰𝙳𝙴 𝙱𝚈 𝙰�
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=t6YXgCpb#jqSO4gfPhneo7G2BqxoZnabbbkxDLBUymsuF1hUk4gY' : process.env.SESSION_ID
 };
